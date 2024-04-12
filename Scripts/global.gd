a71286682b8b595b25a1c80ae80c12ea383bc20e
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-04-11 21:11:55 -0400

    Created global.gd autoload script.
    Created collectible1 (WIP).

---
+++ Scripts/global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var healthFoodCount: int = 0;
+var maxHealthFoodCount: int = 0;
+var goalReached = false;
+
+func reset_collection():
+	healthFoodCount = 0;
+	maxHealthFoodCount = 0;
+	goalReached = false;
