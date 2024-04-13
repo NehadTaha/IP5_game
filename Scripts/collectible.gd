4ca1a6fdb091ee05abfd6150204d48443e822978
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-04-13 17:31:41 -0400

    Added chomp sound effect.
    Added logic for on_body_entered.
    Added collected animation.

--- Scripts/collectible.gd
+++ Scripts/collectible.gd
@@ -1,8 +1,17 @@
 extends Area2D
 
 @onready var animator: AnimationPlayer = $AnimationPlayer
-@onready var collectSound: AudioStreamPlayer3D = $AudioStreamPlayer3D
+@onready var collectSound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 var collected = false;
 
 func _ready():
 	Global.maxHealthFoodCount += 1;
+
+
+func _on_body_entered(body):
+	if(!collected):
+		collected = true;
+		Global.healthFoodCount += 1;
+		collectSound.play()
+		animator.play("collected")
+	pass # Replace with function body.
