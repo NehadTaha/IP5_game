f8bdc1865500dad46ff788998e8fdab510f061be
Nehad Taha <le2125697@crc-lennox.qc.ca>
2024-04-19 22:25:37 -0400

    merge

--- Scripts/collectible.gd
+++ Scripts/collectible.gd
@@ -5,14 +5,14 @@ extends Area2D
 var collected = false;
 
 func _ready():
-	Global.maxHealthFoodCount += 1;
+	#Global.maxHealthFoodCount += 1;
 	animator.play("idle");
 
 
 func _on_body_entered(body):
 	if(!collected):
 		collected = true;
-		Global.healthFoodCount += 1;
+		#Global.healthFoodCount += 1;
 		collectSound.play()
 		animator.play("collected")
 	pass # Replace with function body.
