7d6f5c1fd98cbfe04288a2296a149fdcadca2cf4
NehadTaha <nehad.m.taha@gmail.com>
2024-05-02 13:27:09 -0400

    Menus

---
+++ Scenes/main_screen.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MenuMusic.play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
+
+
+func _on_play_button_button_up():
+	get_tree().change_scene_to_file("res://Scenes/test.tscn");
+
+func _on_button_up():
+	
+	get_tree().quit()
+
+func _on_quit_button_button_down():
+	$ClickedButton.play()
+
+
+func _on_play_button_button_down():
+	$ClickedButton.play()
