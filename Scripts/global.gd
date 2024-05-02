6a672d9dbec61743d3f250e921e4229fa58fb278
LE2088826 <le2088826@crc-lennox.qc.ca>
2024-05-02 17:40:04 -0400

    Added function to return score/total for collectible progress.

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -8,3 +8,7 @@ func reset_collection():
 	healthFoodCount = 0;
 	maxHealthFoodCount = 0;
 	goalReached = false;
+
+# Return an array with a numerator/denominator pair representing the bananas collected
+func get_Collection_Progress():
+	return [healthFoodCount, maxHealthFoodCount]
