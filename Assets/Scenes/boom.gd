b0525162f388e28ae16a0267e25b00738dbaa1a6
Alex <90349909+Hoookpoook@users.noreply.github.com>
2024-05-20 15:10:34 -0400

    level transition

--- Assets/Scenes/boom.gd
+++ Assets/Scenes/boom.gd
@@ -3,13 +3,18 @@ extends GPUParticles2D
 var isPlaying = false
 
 func _on_area_2d_body_entered(body):
-	print("nice")
-	if(!isPlaying):
+	if(!isPlaying):#&& global.healthFoodCount >= global.maxHealthFoodCount
 		set_emitting(true)
 		isPlaying = true
+		$AnimationPlayer.play("win_wait_time")
 		print(isPlaying)
 
 
 func _on_finished():
 	isPlaying = false
 	print(isPlaying)
+
+
+func _on_animation_player_animation_finished(anim_name):
+	global.currentLevel = global.currentLevel + 1
+	get_tree().change_scene_to_packed(global.levelPaths[global.currentLevel])
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,7 +3,8 @@ extends Node
 var healthFoodCount: int = 0;
 var maxHealthFoodCount: int = 0;
 var goalReached = false;
-var currentLevel: int = 1
+var currentLevel: int = 0
+var levelPaths = [preload("res://Assets/Scenes/Level 1.tscn"),preload("res://Assets/Scenes/Level 2.tscn"),preload("res://Assets/Scenes/Level 3.tscn")]
 
 func reset_collection():
 	healthFoodCount = 0;
